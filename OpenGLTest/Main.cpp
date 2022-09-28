8e016cbc50c537656d18b9e48ff0635469f6d9b2
Jonas de Bruin <jonasdebruin05@gmail.com>
2022-09-28 19:06:07 +0200

    i forgor

--- OpenGLTest/Main.cpp
+++ OpenGLTest/Main.cpp
@@ -2,37 +2,56 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;";
+
+const char* fragmentShaderSource;
+
+int windowHeight = 800;
+int windowWidth = 800;
+
 int main()
 {
+	///Init program
 	glfwInit();
+
+
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Epik OpenGLEngine", NULL, NULL);
+	//GLfloat 
+
+	//Create GLFWwindow
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Epik OpenGLEngine", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
 	glfwMakeContextCurrent(window);
 
 	gladLoadGL();
 
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, windowWidth, windowHeight);
 
 	glClearColor(0.04f, 0.13f, 0.17, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	///Run programm
 	glfwSwapBuffers(window);
 
+	//Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 	}
 
-
+	//End programm
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
