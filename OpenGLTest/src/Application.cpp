0dc8a676117d02c721075e6901ceac8ea9e761ed
Jonas de Bruin <jonasdebruin05@gmail.com>
2023-02-03 13:42:39 +0100

    Added glew

--- OpenGLTest/src/Application.cpp
+++ OpenGLTest/src/Application.cpp
@@ -1,5 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -7,6 +9,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,6 +22,11 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+	    std::cout << "Glew is not OK" << std::endl;
+
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
